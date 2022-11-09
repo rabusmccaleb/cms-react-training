f69ffbe03d21a45ef52239f0c668f17f6c218025
Rabus McCaleb <mccalebrabus@gmail.com>
2022-11-09 11:25:59 -0700

    pushing changes to work on full project

--- pages/components/Comic.tsx
+++ pages/components/Comic.tsx
@@ -7,14 +7,14 @@ type Prop = {
 	thumbnail? : string,
 	issueNumber? : string,
 	publishDate? : Date,
-	creators : {}[],
+	creators : {},
 }
 
 const Comic = ({thumbnail, title, issueNumber, publishDate, creators} : Prop) => {
 	let creatorsList = creators.items.map(creator => creator.name);
-	const isoDate = new Date(publishDate.slice(0, -1));
-	const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-	const date = `${ months[isoDate.getMonth()] } ${ isoDate.getDay() }, ${ isoDate.getFullYear() }`;
+	// const isoDate = new Date(publishDate.slice(0, -1));
+	// const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+	const date = publishDate; //`${ months[isoDate.getMonth()] } ${ isoDate.getDay() }, ${ isoDate.getFullYear() }`;
 	return(
 		<div className={styles.comicConatiner}>
 			<Image src={thumbnail} width={158} height={263} className={styles.comicThumbnail}/>
@@ -23,18 +23,25 @@ const Comic = ({thumbnail, title, issueNumber, publishDate, creators} : Prop) =>
 			</div>
             <div className={styles.infoContainer}>
                 <h4 className={styles.comicTitle}>{title}</h4>
-                <div className={styles.comicLabelContainers}>
-                    <label className={styles.comicLabelTitle}>Issue Number : </label>
-                    <label>{` ${issueNumber}`}</label>
-                </div>
-                <div className={styles.comicLabelContainers}>
-                    <label className={styles.comicLabelTitle}>Published : </label>
-                    <label>{` ${date}`}</label>
-                </div>
-                <div className={`${styles.comicLabelContainers} ${styles.creators}`}>
-                    <label className={styles.comicLabelTitle}>Creators : </label> 
-                    <label>{` ${creatorsList.join(', ')}`}</label>
-                </div>
+				{ (issueNumber && issueNumber.trim() !== '') &&
+					<div className={styles.comicLabelContainers}>
+						<label className={styles.comicLabelTitle}>Issue Number : </label>
+						<label>{` ${issueNumber}`}</label>
+					</div>
+				}
+
+                { publishDate  &&
+					<div className={styles.comicLabelContainers}>
+                    	<label className={styles.comicLabelTitle}>Published : </label>
+                    	<label>{` ${date}`}</label>
+                	</div>
+				}
+				{ (creatorsList && creatorsList.length > 0) &&
+					<div className={`${styles.comicLabelContainers} ${styles.creators}`}>
+						<label className={styles.comicLabelTitle}>Creators : </label> 
+						<label>{` ${creatorsList.join(', ')}`}</label>
+					</div>
+				}
             </div>
 		</div>
 	)
